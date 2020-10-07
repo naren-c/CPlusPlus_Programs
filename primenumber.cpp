08699a4508e6e5d9433a44cba493249c693bb689
naren-c <naren.c106@gmail.com>
2020-10-07 14:04:08 +0530

    PrimeNumber

--- primenumber.cpp
+++ primenumber.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
-/* void main()
+int main()
 {
-    cout<<"hello";
-} */
+    int p,i,isprime = 1;
+    cout<<"Enter a positive integer"<<endl;
+    cin>>p;
+    //cout << p;
+    if(p==1 || p==0)
+    {
+        cout<<p<<"not a prime number";
+    }
+    else
+    {
+        for(i=2;i<=p-1;i++)
+        {
+            if(p%i==0)
+            {
+                cout<<p<<"is not a prime number";
+                isprime = 0;
+                break;
+            }
+        }
+        if(isprime == 1)
+            cout<<p<<"is a prime number";
+    }
+}
